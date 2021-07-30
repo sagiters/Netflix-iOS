474ba14d26114b11117f31d8cd63abdafeb200db
Jack <sagiters19@gmail.com>
2021-07-31 01:21:50 +0800

    Complete searchView and coming soon view

---
+++ Netflix/Netflix/iOS/ComingSoon/NotificationBar.swift
@@ -0,0 +1,40 @@
+//
+//  NotificationBar.swift
+//  Netflix
+//
+//  Created by Lien-Tai Kuo on 2021/7/31.
+//
+
+import SwiftUI
+
+struct NotificationBar: View {
+
+    @Binding var showNotificationList: Bool
+
+    var body: some View {
+        Button(action: {
+            showNotificationList = true
+        }, label: {
+            HStack {
+                Image(systemName: "bell")
+                Text("Notifications")
+                    .bold()
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            .font(.system(size: 18, weight: .bold))
+        })
+        .foregroundColor(.white)
+        .padding()
+    }
+}
+
+struct NotificationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            NotificationBar(showNotificationList: .constant(false))
+        }
+    }
+}
--- Netflix/Netflix/iOS/Helpers/GlobalHelpers.swift
+++ Netflix/Netflix/iOS/Helpers/GlobalHelpers.swift
@@ -200,3 +200,9 @@ extension String {
         return size.width
     }
 }
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
